564909aedbc97e2e854bee53edf5ed75cfc6ad7f
BekenJB <bekentyjb45@aol.com>
2017-10-03 23:11:45 -0400

    To be use as a demo how to upload images into azure blob container

--- UploadToAzureContainer/Controllers/HomeController.cs
+++ UploadToAzureContainer/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace UploadToAzureContainer.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+             
             return View();
         }
 
